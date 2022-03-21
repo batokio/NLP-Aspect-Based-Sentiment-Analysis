3f9b21bc3311c3509549630e20a1f3dab892dbe4
batokio <76013828+batokio@users.noreply.github.com>
2022-03-21 18:56:58 +0100

    Update imports.py

--- src/imports.py
+++ src/imports.py
@@ -13,21 +13,13 @@ from tqdm import tqdm
 
 import numpy as np
 import pandas as pd
-# from pylab import rcParams
-# import matplotlib.pyplot as plt
-# from sklearn.model_selection import train_test_split
 from collections import defaultdict
-# from textwrap import wrap
 
 from torch import nn, optim
 from torch.utils.data import Dataset, DataLoader
 from torch.optim import lr_scheduler
 import torch.nn.functional as F
 import random as rn
-
-# from nltk.tokenize import word_tokenize
-# from nltk.tokenize.treebank import TreebankWordDetokenizer
-# import re
 
 np.random.seed(17)
 rn.seed(12345)
